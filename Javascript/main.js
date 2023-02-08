9573939ac07e1a49a10a0ab4931feb9a031f9967
Wolfy4H <Leon.Kersten@outlook.com>
2023-02-08 08:54:28 +0100

    smooth scroll

--- Javascript/main.js
+++ Javascript/main.js
@@ -6,12 +6,18 @@ menuBtn.onclick = () => {
   navbar.classList.add("show");
   menuBtn.classList.add("hide");
   body.classList.add("disabled");
-}
+};
 cancelBtn.onclick = () => {
   body.classList.remove("disabled");
   navbar.classList.remove("show");
   menuBtn.classList.remove("hide");
-}
+};
 window.onscroll = () => {
-  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
+  this.scrollY > 20
+    ? navbar.classList.add("sticky")
+    : navbar.classList.remove("sticky");
+};
+
+//smooth scroll
+var element = document.querySelector("#locate");
+element.scrollIntoView({ behavior: "smooth", block: "end" });
